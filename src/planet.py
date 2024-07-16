5dad962eb6d2ccd6931213b4397af225f021136d
elenagonzalez870 <elenagonzalez870@gmail.com>
2024-07-16 17:12:22 -0500

    Adding script that calculates all of the relative positions and adding attributes to the planet class

--- src/planet.py
+++ src/planet.py
@@ -1,8 +1,10 @@
 class Planet():
-    def __init__(self, label, period, radius, distance, tilt):
+    def __init__(self, label, period, radius, distance, tilt, ra, dec):
         self.label = label
         self.period = period
         self.radius = radius
         self.distance = distance
         self.tilt = tilt
+        self.ra = ra
+        self.dec = dec
         
