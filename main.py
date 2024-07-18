f3cbd583a000bde85b7ac69e96f197386d059b46
Corrado777 <102477481+Corrado777@users.noreply.github.com>
2024-07-18 11:34:24 -0500

    started main interface script

---
+++ main.py
@@ -0,0 +1,12 @@
+import src as src
+
+def main():
+    planet_name = input("Enter the name of the planet where the observatory will be located: ")
+    assert planet_name =='Mars', 'The planet name is not Mars, please enter the correct planet name'
+
+    # Get the planet info given the planet label
+    
+
+
+if __name__ == '__main__':
+    main()
