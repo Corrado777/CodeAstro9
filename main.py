1050ce585914f0ea7cebf7f12fd48d8bed0a0d2f
Corrado777 <102477481+Corrado777@users.noreply.github.com>
2024-07-18 13:30:18 -0500

    work on main

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ def main():
     planet_name = input("Enter the name of the planet where the observatory will be located: ")
     assert planet_name =='Mars', 'The planet name is not Mars, please enter the correct planet name'
 
-    # Grab the data and initialize a planet object given the planet name
-
 
     # Prompt the user to enter observatory latitude and Longitude
     location = input("Enter observatory latitude and longitude separated by commas (e.g., value1,value2): ")
@@ -14,6 +12,18 @@ def main():
     latitude, longitude = tuple(location.split(','))
 
     print(latitude, longitude)  # This will print the tuple of values
+
+    # initialize a new observatory object with the latitude and longitude
+    observatory = src.Observatory(latitude, longitude)
+
+    date_string = input("Enter the date in the format YYYY-MM-DD: ")
+
+    planet = src.Planet()
+    planet_data = src.load_planet(planet_name, date_string)
+    # planet location on celestial sphere as seen from Earth
+    planet.ra = planet_data['RA']
+    planet.dec = planet_data['DEC']
+
 
 
 if __name__ == '__main__':
