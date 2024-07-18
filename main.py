8c0ad9735dbcfa8fb82c90aae1c51f1a9243aff5
Corrado777 <102477481+Corrado777@users.noreply.github.com>
2024-07-18 13:18:16 -0500

    update main

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ def main():
     planet_name = input("Enter the name of the planet where the observatory will be located: ")
     assert planet_name =='Mars', 'The planet name is not Mars, please enter the correct planet name'
 
-    # Get the planet info given the planet label
-    
+    # Grab the data and initialize a planet object given the planet name
+
+
+    # Prompt the user to enter observatory latitude and Longitude
+    location = input("Enter observatory latitude and longitude separated by commas (e.g., value1,value2): ")
+
+    # Convert the input string to a tuple by splitting the string and then using tuple() to convert the list to a tuple
+    latitude, longitude = tuple(location.split(','))
+
+    print(latitude, longitude)  # This will print the tuple of values
 
 
 if __name__ == '__main__':
